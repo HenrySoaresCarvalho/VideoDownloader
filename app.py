72f2f312ceb1ea0e6c5ccce71b78319ec2168e36
HenrySoaresCarvalho <henrysoarescarvalho@gmail.com>
2021-03-17 18:13:32 -0300

    Added Info Route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 try:
     from pytube import YouTube
-    from flask import Flask,jsonify,send_file
+    from flask import Flask,jsonify,request,send_file
 
 except:
     from packageInstaller import package_installer
@@ -17,6 +17,22 @@ def index():
         "creator":"Henry Soares de Carvalho",
         "version": 0.1
     })
+
+@app.route("/video/info", methods=["POST"])
+def get_info():
+	try:
+		req = request.get_json()
+		yt = YouTube(req["link"])
+		return jsonify({
+			"video_name": yt.title,
+			"video_views": yt.views,
+			"author":yt.author
+		})
+	except:
+		return jsonify({
+			"message":"error"
+		})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
